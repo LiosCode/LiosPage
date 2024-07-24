170b90e3f6c42d9b37e57cd9de060c4e8d53f79e
LiosCode <126961749+LiosCode@users.noreply.github.com>
2024-07-24 17:19:08 +0200

    Login seite/ UX eingerichtet

--- website/src/app/components/navbar.tsx
+++ website/src/app/components/navbar.tsx
@@ -1,34 +1,45 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.jsx";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+// import { AcmeLogo } from "./AcmeLogo.jsx";
+import Image from "next/image";
 
-export default function navbar() {
+export default function App() {
   return (
     <Navbar>
       <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        
+        {/* Füge hier ein Bild ein für das Seiten Logo */}
+        <div className="ml-4">
+          <Image
+            src="/OhneTitel2.svg" // Ersetze dies durch den Pfad zu deinem Bild
+            alt="Logo"
+            className="dark:invert"
+            width={50} // Setze die gewünschte Breite
+            height={50} // Setze die gewünschte Höhe
+          />
+        </div>
+        <p className="font-bold text-inherit">LiosCodeX</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
+            Tickets
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Customers
+            Gutscheine
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Integrations
+            FanShop
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="login">Login</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">

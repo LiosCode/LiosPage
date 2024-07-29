8005c33009d0aa492e8824b548386d01a9985384
LiosCode <126961749+LiosCode@users.noreply.github.com>
2024-07-29 17:13:05 +0200

    Logo wieder ins Center gebracht

--- website/src/app/login/page.tsx
+++ website/src/app/login/page.tsx
@@ -66,15 +66,22 @@ const NextLoginPage = () => {
   return (
     sessionStatus !== "authenticated" && (
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+         <div className="flex justify-center">
         <Link href="/">
             <Image 
               src="/OhneTitel2.svg" 
-              alt="logo" 
+              alt="logo"
+               
               className="dark:invert cursor-pointer" 
               width={50} 
               height={50} 
             />
+            
           </Link>
+          </div>
+          <h2 className="mt-6 text-center text-2xl leading-9 tracking-tight text-slate-700">
+            Sign in to your account
+          </h2>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
             <form className="space-y-6" onSubmit={handleSubmit}>

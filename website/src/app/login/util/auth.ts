2ff69a5e9b9e6623af2c3ddb510256e90d891a82
LiosCode <126961749+LiosCode@users.noreply.github.com>
2024-07-30 19:00:09 +0200

    Einrichten Firebase

---
+++ website/src/app/login/util/auth.ts
@@ -0,0 +1,33 @@
+// auth.ts
+import { auth } from "./firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export const registerWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loginWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    throw error;
+  }
+};
